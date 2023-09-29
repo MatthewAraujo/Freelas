ffb241cfadb8d88e164ea728fd8b601b8d2eaa69
Matthew Araujo <90223014+MatthewAraujo@users.noreply.github.com>
2023-09-29 14:26:43 -0300

    Update footer.tsx

--- src/pages/components/Footer/footer.tsx
+++ src/pages/components/Footer/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="w-full flex justify-end items-end  text-grayText">
       <div className=" text-base ">
         <a href="https://github.com/MatthewAraujo" target="_blank" className="w-full">
-          Created by Matthew araujo
+          Created by Architek
         </a>
       </div>
     </footer>
